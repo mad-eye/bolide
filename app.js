6a86a106816a78dcc236bed0cf42707bf27559f1
Mike Risse <rissem@gmail.com>
2012-09-27 08:03:31 -0700

    add a sharejs backed ace editor to the project

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var app = express();
+var sharejs = require('share').server;
+
+var options = {db: {type: 'redis'}};
+sharejs.attach(app, options);
 
 app.get('/', function(req, res){
   res.send('Hello World');
 });
+app.use(express.static(__dirname + '/public'));
 
 app.listen(3000);
 console.log('Listening on port 3000');
