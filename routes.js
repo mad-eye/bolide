00ff7a544401603ee7c84708c9ec211f0281543c
James Gill <jamesagill@gmail.com>
2012-10-02 11:07:35 -0700

    Using child_process to issue a shell command.

--- routes.js
+++ routes.js
@@ -1,32 +1,51 @@
 (function () {
 
-var GitHubApi = require("github");
-var github = new GitHubApi({
+  var GitHubApi = require("github");
+  var github = new GitHubApi({
     version: "3.0.0"
-});
+  });
 
-var routes = module.exports = function(app) {
+  var sys = require('sys');
+  var exec = require('child_process').exec;
+
+  var routes = module.exports = function(app) {
 
     app.get('/', function(req, res){
-        res.send('Hello World');
+      res.send('Hello World');
     });
 
     app.get('/github', function(req, res){
-        github.repos.get({
-            user: "fgnass",
-            repo: "node-dev"
-        }, function(err, res) {
-            if (err) {
-                console.error(JSON.stringify(err));
-            } else {
-                var gitUrl = res["git_url"];
-                console.log(gitUrl);
-            }
-        });
-        var path = __dirname + "/views/github.jade";
-        res.render(path, {});
+      var path = __dirname + "/views/github.jade";
+      res.render(path, {});
     });
 
-};
+    app.post('/clone', function(req, res) {
+      var resBody = "";
+      var gitUrl;
+      github.repos.get({
+        user: "fgnass",
+        repo: "node-dev"
+      }, function(err, ghResponse) {
+        if (err) {
+          console.error(JSON.stringify(err));
+          res.send("ERROR: " + JSON.stringify(err));              
+        } else {
+          gitUrl = ghResponse["git_url"];
+          resBody += 'Cloning ' + gitUrl;
+          var puts = function (error, stdout, stderr) {
+            console.log("STDOUT: " + stdout);
+            console.log("STDERR: " + stderr);
+            if (error) {
+              res.send("ERROR: " + error);              
+            } else {
+              res.send("Cloned " + gitUrl);              
+            }
+          };
+          exec("ls -la", puts);
+        }
+      });
+    });
+
+  };
 
 })();
