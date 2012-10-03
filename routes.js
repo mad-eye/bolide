e840c3e54a619b1976bcef392481f01da1ac8cf4
James Gill <jamesagill@gmail.com>
2012-10-02 17:47:29 -0700

    Cloning a git repo with a button.

--- routes.js
+++ routes.js
@@ -7,6 +7,39 @@
 
   var sys = require('sys');
   var exec = require('child_process').exec;
+  var uuid = require('node-uuid');
+
+  var rootDir = "/tmp/bolide/repoClones/";
+
+  function handleError(error, res) {
+    console.error("ERROR: " + JSON.stringify(error));
+    res.send("There was error: " + JSON.stringify(error));
+  }
+
+
+  function handleGitubUrlResponse(err, ghResponse, res) {
+    if (err) {
+      handleError(err, res);
+    } else {
+      exec('mkdir -p "' + rootDir + '"', function (error, stdout, stderr) {
+        if (error) {
+          handleError(error, res);
+        } else {
+          console.log(stdout);
+          var repoDir = rootDir + uuid.v4();
+          exec('git clone ' + ghResponse["git_url"] + ' "' + repoDir + '"', 
+            function (error, stdout, stderr) {
+              if (error) {
+                handleError(error, res);
+              } else {
+                console.log(stdout);
+                res.send("Cloned " + ghResponse["git_url"]);
+              }
+          });
+        }
+      });
+    }
+  }
 
   var routes = module.exports = function(app) {
 
@@ -20,29 +53,11 @@
     });
 
     app.post('/clone', function(req, res) {
-      var resBody = "";
-      var gitUrl;
       github.repos.get({
         user: "fgnass",
         repo: "node-dev"
       }, function(err, ghResponse) {
-        if (err) {
-          console.error(JSON.stringify(err));
-          res.send("ERROR: " + JSON.stringify(err));              
-        } else {
-          gitUrl = ghResponse["git_url"];
-          resBody += 'Cloning ' + gitUrl;
-          var puts = function (error, stdout, stderr) {
-            console.log("STDOUT: " + stdout);
-            console.log("STDERR: " + stderr);
-            if (error) {
-              res.send("ERROR: " + error);              
-            } else {
-              res.send("Cloned " + gitUrl);              
-            }
-          };
-          exec("ls -la", puts);
-        }
+        handleGitubUrlResponse(err, ghResponse, res);
       });
     });
 
