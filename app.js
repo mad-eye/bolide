933006949c0923c407f201adfdfb0aeb11084c9c
rissem <rissem@gmail.com>
2012-12-10 17:52:54 -0800

    remove some cruft from the bolide project

--- app.js
+++ app.js
@@ -4,22 +4,5 @@ var sharejs = require('share').server;
 
 var options = {db: {type: 'redis'}, browserChannel:{cors: "*"}};
 sharejs.attach(app, options);
-
-//Put routing in routs.js
-require('./routes')(app);
-
-//app.get('/', function(req, res){
-//  res.send('Hello World');
-//});
-
-//app.get('/github', function(req, res){
-//  //res.send('Hello Github!' + __dirname);
-//  var path = __dirname + "/views/github.jade";
-//  console.log("Rendering " + path);
-//  res.render(path, {});
-//});
-
-app.use(express.static(__dirname + '/public'));
-
 app.listen(3003);
 console.log('Listening on port 3003');
