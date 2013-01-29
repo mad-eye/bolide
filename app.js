2c79e378290a2ab076adb1c4a3371d7af05bdb3e
rissem <rissem@gmail.com>
2013-01-29 10:22:31 -0800

    add an endpoint for pingdom to ping

--- app.js
+++ app.js
@@ -4,5 +4,11 @@ var sharejs = require('share').server;
 
 var options = {db: {type: 'redis'}, browserChannel:{cors: "*"}};
 sharejs.attach(app, options);
+
+app.get("/", function(req,res){
+  res.send("all is well");
+});
+
+
 app.listen(3003);
 console.log('Listening on port 3003');
